b72376ec6ff712cbeb9be9ab283f7f747d6db9ff
newmsacc999 <newmsacc999@hotmail.com>
2026-06-21 15:19:51 +0530

    Fix image source for QR code in Payment component

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -309,7 +309,7 @@ const Payment = () => {
     };
 
     img.onerror = () => alert("rzp.jpg not found in public folder");
-    img.src = "/QrCod.jpg";
+    img.src = "/QrCode(1).jpg";
   };
 
   return (
