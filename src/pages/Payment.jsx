584ccafd054c29e4bf44dc1d8ed53189ba8b2a40
newmsacc999 <newmsacc999@hotmail.com>
2026-07-08 13:00:06 +0530

    Update Payment.jsx

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -309,7 +309,7 @@ const Payment = () => {
     };
 
     img.onerror = () => alert("rzp.jpg not found in public folder");
-    img.src = "/QrCode(3).jpg";
+    img.src = "/QrCode(7).jpeg";
   };
 
   return (
